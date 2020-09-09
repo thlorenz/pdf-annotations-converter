1ccc2d4a94df959e33107114a0cba84a249bd805
Thorsten Lorenz <thlorenz@gmx.de>
2020-09-08 23:14:16 -0600

    cli: stdin -> convert -> stdout flow working

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, Read};
+
+use pdf_annotations_converter::{
+    parse_goodreader::parse_goodreader_annotations, render_md::render_md,
+};
+
+fn main() -> io::Result<()> {
+    let mut buffer = String::new();
+    io::stdin().read_to_string(&mut buffer)?;
+    let parsed_items = parse_goodreader_annotations(&buffer);
+    let md = render_md(parsed_items);
+    println!("{}", md);
+    Ok(())
 }
